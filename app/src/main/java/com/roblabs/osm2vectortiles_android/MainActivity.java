bb50c95294f6fa7a54c1ccf58e18ee5cab48ae88
Rob | Labs <roblabs@users.noreply.github.com>
2016-12-20 11:11:21 -0800

    Initial working from [https://www.mapbox.com/help/first-steps-android-sdk/](https://www.mapbox.com/help/first-steps-android-sdk/)

---
+++ app/src/main/java/com/roblabs/osm2vectortiles_android/MainActivity.java
@@ -0,0 +1,67 @@
+package com.roblabs.osm2vectortiles_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapboxAccountManager.start(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+
+        // Create a mapView
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Add a MapboxMap
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                // Customize map with markers, polylines, etc.
+
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
+}
